4b2e5d7037943307c208a6326653e6e3ec5f9ae7
TaniaAlexBM <lilith.aglariment@gmail.com>
2022-06-01 09:22:14 -0500

    finish package

--- pythonPackageCodigofacilito/__main__.py
+++ pythonPackageCodigofacilito/__main__.py
@@ -1,8 +1,18 @@
 # archivo para ejecutar el paquete sin necesidad de llamarlo en un archivo
 
+import logging
+'''
+    Imprime diferentes tipos de mensajes:
+        info        10
+        debug       20
+        warning     30
+        error       40
+        critical    50
+'''
 from pythonPackageCodigofacilito import unreleased
 
+logging.basicConfig(level=logging.DEBUG)
+
 if __name__ == '__main__':
-    print('Iniciando la ejecución del paquete')
     workshops = unreleased()
-    print(workshops)
+    logging.debug(workshops)
